f92994b1bb6edeb2c93220817de543b2f6e92ebc
what'up <tunkrawaysoe2002@gmail.com>
2024-11-12 20:44:47 +0630

    generate html for checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export const cart = [];
+export let cart = [{
+  productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity:2
+},
+{
+  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity:1
+}
+
+
+];
 
 export function addToCart(productId,selectValue){
     let matchingItem;
@@ -20,4 +30,16 @@ export function addToCart(productId,selectValue){
   
   }
   
+   }
+   export function removeFromCart(productId){
+    const newCart=[];
+    cart.forEach((cartItem)=>{
+      if(!productId===cartItem.productId){
+        newCart.push(cartItem)
+      }
+    });
+    newCart=cart;
+
+
+
    }
